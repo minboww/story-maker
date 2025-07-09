01613b09bbbd35bcf79020f4b3a0d36d5eff3272
minboww <142311565+minboww@users.noreply.github.com>
2025-07-10 01:53:03 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const PROJECT_ID = process.env.GCP_PROJECT_ID; 
 const LOCATION = 'us-central1';
 const vertex_ai = new VertexAI({ project: PROJECT_ID, location: LOCATION });
-const generativeModel = vertex_ai.getGenerativeModel({ model: ' gemini-1.5-flash' });
+const generativeModel = vertex_ai.getGenerativeModel({ model: 'gemini-2.0-flash-001' });
 const generativeVisionModel = vertex_ai.getGenerativeModel({ model: 'imagegeneration@0.0.2' });
 
 // --- APIエンドポイント ---
